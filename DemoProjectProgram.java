079dee8810be62bc74557eac14b5e9b1c70b2e78
Aftaab007 <aftaabahmed79979@gmail.com>
2025-01-23 12:48:40 +0530

    v3

---
+++ DemoProjectProgram.java
@@ -0,0 +1,11 @@
+
+
+public class DemoProjectProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Welcome");
+		System.out.println("Hyderabad");
+	}
+
+}
